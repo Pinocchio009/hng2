af5782665dcff25f45c490a3f3e3c6274d5375d7
Pinocchio <officialnikx@gmail.com>
2022-11-04 22:50:34 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const {body, validationResult} = require('express-validator')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
+
+app.post('/', body('x').isInt(), body('y').isInt(), (req, res)=>{
+ 
+  const x = req.body.x
+  const y = req.body.y
+  const operator = req.body.operation_type
+  const operatorEnum = ['addition', 'substraction', 'multiplication']
+  let result
+  let index
+
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array()})
+  }
+  if(operatorEnum.includes(operator)){
+    if(operator === 'addition'){
+      index = 1
+      result = x + y
+    }else if (operator === 'substraction'){
+      index =1
+      result = x-y
+    }else if (operator === 'multiplication'){
+      result = x*y
+      index = 2
+    }
+    return res.status(200).json({
+      slackUsername: 'Pinocchio',
+      operation_type: operatorEnum[index],
+      result
+    })
+  }
+})
+app.listen(PORT,()=>{
+  console.log(`serving on port ${PORT}`)
+})
